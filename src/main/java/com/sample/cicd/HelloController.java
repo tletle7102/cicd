1b9e40a9aeb6a8d752f4a2faa9429da04ec77100
tletle7102 <tletle7102@gmail.com>
2025-03-21 08:52:42 +0900

    Update hello message

--- src/main/java/com/sample/cicd/HelloController.java
+++ src/main/java/com/sample/cicd/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, CI/CD World!";
+        return "Hello, CI/CD World! Updated!";
     }
 }
